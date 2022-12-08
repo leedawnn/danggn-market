ba4f7776d7b4a039c510a518a317844295ca22fa
leedawnn <2g_hee@naver.com>
2022-12-08 23:51:03 +0900

    feat: 버튼 공통 컴포넌트 구현

---
+++ src/components/commons/buttons/01/index.tsx
@@ -0,0 +1,40 @@
+import styled from '@emotion/styled';
+import Link from 'next/link';
+
+interface IButtonProps {
+  title: string;
+  url: string;
+}
+
+const Button01 = (props: IButtonProps) => {
+  return (
+    <Button>
+      <ButtonText>
+        <Link href={props.url}>
+          <a>{props.title}</a>
+        </Link>
+      </ButtonText>
+    </Button>
+  );
+};
+export default Button01;
+
+const Button = styled.button`
+  background-color: #f1f3f5;
+  padding: 1.4rem 2rem;
+  margin-top: 1.5rem;
+  border-radius: 6px;
+  cursor: pointer;
+
+  :hover {
+    background-color: #e9ecef;
+  }
+`;
+
+const ButtonText = styled.span`
+  color: #212529;
+  font-size: 1.1rem;
+  font-weight: 700;
+  line-height: 1.47;
+  letter-spacing: -0.3px;
+`;
