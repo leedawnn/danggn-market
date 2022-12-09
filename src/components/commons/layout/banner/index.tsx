5c78c2282ca05ef0f5af19c7f23af227d4cbb0df
leedawnn <2g_hee@naver.com>
2022-12-09 17:29:16 +0900

    feat: 배너에 댕근마켓 소개 문구 추가

--- src/components/commons/layout/banner/index.tsx
+++ src/components/commons/layout/banner/index.tsx
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import Link from 'next/link';
 
 const Banner = () => {
   const settings = {
@@ -11,7 +12,7 @@ const Banner = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 4000,
+    autoplaySpeed: 3500,
     cssEase: 'ease-in-out',
     useTransform: false,
   };
@@ -22,6 +23,18 @@ const Banner = () => {
         <SliderItem src='/2.jpeg' />
         <SliderItem src='/1.jpeg' />
       </Slider>
+      <SlideContent>
+        <ContentTitle>국내 최초 반려동물만을 위한 중고마켓</ContentTitle>
+        <Content>
+          중고 거래부터 반려동물 정보까지, 반려인과 함께해요.
+          <br /> 내 새꾸 자랑도 하고 꿀팁도 얻어가세요 :)
+        </Content>
+        <Link href='/market/create'>
+          <a>
+            <MoveToMarketBtn>판매하기</MoveToMarketBtn>
+          </a>
+        </Link>
+      </SlideContent>
     </Wrapper>
   );
 };
@@ -36,4 +49,35 @@ const Wrapper = styled.div`
 const SliderItem = styled.img`
   width: 100%;
   height: 100vh;
+`;
+
+const SlideContent = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  animation: focus-in-expand-fwd 0.4s linear both;
+  text-align: center;
+`;
+
+const ContentTitle = styled.span`
+  color: #ffffff;
+  font-size: 36px;
+  font-weight: 700;
+  letter-spacing: 3px;
+`;
+
+const Content = styled.p`
+  color: #ffffff;
+  margin: 1rem 0;
+`;
+
+const MoveToMarketBtn = styled.button`
+  width: 120px;
+  color: #ffffff;
+  border: 1px solid #ffffff;
+  padding: 1rem;
+  background-color: rgba(255, 255, 255, 0);
+  margin-top: 1rem;
+  cursor: pointer;
 `;
