a08ed6a1476a9fc640a7c3e21563faf7dc8c8952
leedawnn <2g_hee@naver.com>
2022-12-14 18:09:46 +0900

    styles: 카카오 버튼일 경우 노란색으로 변경

--- src/components/commons/buttons/02/index.tsx
+++ src/components/commons/buttons/02/index.tsx
@@ -2,21 +2,30 @@ import styled from '@emotion/styled';
 
 interface IButton02Props {
   title: string;
+  kakao: boolean;
   onClickLogin?: () => Promise<void>;
 }
 
-const Button02 = ({ title, onClickLogin }: IButton02Props) => {
-  return <Button onClick={onClickLogin}>{title}</Button>;
+const Button02 = ({ title, kakao, onClickLogin }: IButton02Props) => {
+  return (
+    <Button onClick={onClickLogin} kakao={kakao}>
+      {title}
+    </Button>
+  );
 };
 export default Button02;
 
-const Button = styled.button`
+interface IButtonColorProps {
+  kakao: boolean;
+}
+
+const Button = styled.button<IButtonColorProps>`
   width: 374px;
   height: 58px;
   margin-top: 1rem;
   font-size: 1rem;
-  color: #ffffff;
-  background-color: #000000;
+  color: ${(props) => (props.kakao ? '#000000' : '#ffffff')};
+  background-color: ${(props) => (props.kakao ? '#fef01b' : '#000000')};
   cursor: pointer;
 
   :hover {
