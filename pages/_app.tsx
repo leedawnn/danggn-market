3125af67bfeff2767c5a1dc76fa8ee22002a67c1
leedawnn <2g_hee@naver.com>
2022-12-30 02:50:47 +0900

    feat: 로딩 이미지 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,20 +5,72 @@ import Layout from '../src/components/commons/layout';
 import { AppProps } from 'next/app';
 import ApolloSetting from '../src/commons/settings/apolloSetting';
 import { RecoilRoot } from 'recoil';
+import { useEffect, useState } from 'react';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import Lottie from 'react-lottie';
+import animationData from '../public/animal-paw-loading.json';
+import styled from '@emotion/styled';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
+  useEffect(() => {
+    const start = () => {
+      setLoading(true);
+    };
+
+    const end = () => {
+      setLoading(false);
+    };
+
+    Router.events.on('routeChangeStart', start);
+    Router.events.on('routeChangeComplete', end);
+    Router.events.on('routeChangeError', end);
+
+    return () => {
+      Router.events.off('routeChangeStart', start);
+      Router.events.off('routeChangeComplete', end);
+      Router.events.off('routeChangeError', end);
+    };
+  }, []);
+
   return (
     <>
-      <RecoilRoot>
-        <ApolloSetting>
-          <Global styles={globalStyles} />
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </ApolloSetting>
-      </RecoilRoot>
+      {loading ? (
+        <LoadingWrapper>
+          <Lottie options={defaultOptions} width={300} height={300} />
+        </LoadingWrapper>
+      ) : (
+        <RecoilRoot>
+          <ApolloSetting>
+            <Global styles={globalStyles} />
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </ApolloSetting>
+        </RecoilRoot>
+      )}
     </>
   );
-}
+};
 
 export default MyApp;
+
+const LoadingWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
