f0299bca39dd51e8982e8d2bfc138a49158c06b6
leedawnn <2g_hee@naver.com>
2023-01-13 01:10:08 +0900

    fix: withAuth에 권한 분기 메시지 추가

--- src/commons/libraries/withAuth.tsx
+++ src/commons/libraries/withAuth.tsx
@@ -1,3 +1,4 @@
+import { Modal } from 'antd';
 import { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
@@ -9,6 +10,7 @@ export const withAuth = (Component: any) => (props: AppProps) => {
   useEffect(() => {
     getAccessToken().then((token) => {
       if (!token) {
+        Modal.info({ content: '로그인이 필요합니다!' });
         router.replace('/auth/signin');
       }
     });
