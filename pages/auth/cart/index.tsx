c81792d310dbfca9210958cea258035a539f2617
leedawnn <2g_hee@naver.com>
2023-01-28 22:25:28 +0900

    remove: 필요없는 import 제거

--- pages/auth/cart/index.tsx
+++ pages/auth/cart/index.tsx
@@ -1,9 +1,4 @@
-import styled from '@emotion/styled';
-import { useEffect, useState } from 'react';
 import { withAuth } from '../../../src/commons/libraries/withAuth';
-import { IUseditem } from '../../../src/commons/types/generated/types';
-import { CartState } from '../../../src/commons/store';
-import { useRecoilState } from 'recoil';
 import CartContainer from '../../../src/components/units/auth/cart/Cart.container';
 
 const CartPage = () => {
