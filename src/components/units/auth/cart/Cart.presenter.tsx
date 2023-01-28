0cdb90e86b363df9af646c6b1cd4f3d5d31980cd
leedawnn <2g_hee@naver.com>
2023-01-29 00:39:11 +0900

    styles: 장바구니가 비어있을 경우 문구 추가

--- src/components/units/auth/cart/Cart.presenter.tsx
+++ src/components/units/auth/cart/Cart.presenter.tsx
@@ -1,6 +1,7 @@
+import * as S from './Cart.styles';
+
 import { putOnComma } from '../../../../commons/libraries/utils';
 import { IUseditem } from '../../../../commons/types/generated/types';
-import * as S from './Cart.styles';
 
 interface ICartUIProps {
   baskets: IUseditem[];
@@ -8,6 +9,8 @@ interface ICartUIProps {
 }
 
 const CartUI = ({ baskets, handleImageError }: ICartUIProps) => {
+  const isEmptyBaskets = baskets.length === 0;
+
   return (
     <S.Wrapper>
       <S.CartWrapper>
@@ -15,20 +18,33 @@ const CartUI = ({ baskets, handleImageError }: ICartUIProps) => {
           <h2>장바구니</h2>
         </S.CartHeader>
         <S.CartBody>
-          {baskets.map((el) => (
-            <S.Row key={el._id}>
-              <S.ImageWrapper>
-                <S.ProductImage
-                  key={el._id}
-                  src={`https://storage.googleapis.com/${el.images?.[0]}`}
-                  onError={handleImageError}
-                />
-              </S.ImageWrapper>
-              <S.Column>{el.name}</S.Column>
-              <S.ProductPrice>{putOnComma(el.price)}원</S.ProductPrice>
-              <S.Column>{el.remarks}</S.Column>
-            </S.Row>
-          ))}
+          {isEmptyBaskets ? (
+            <S.EmptyCartWrapper>
+              <S.EmptyCartIcon />
+              <S.EmptyTitle>EMPTY</S.EmptyTitle>
+              <S.DivideLine />
+              <S.EmptyCartSpan>NO ITEM IN SHOPPING CART</S.EmptyCartSpan>
+              <S.EmptyCartSpan>
+                장바구니가 비어있습니다. <br />
+                선택하신 상품을 장바구니에 담아주세요.
+              </S.EmptyCartSpan>
+            </S.EmptyCartWrapper>
+          ) : (
+            baskets.map((el) => (
+              <S.Row key={el._id}>
+                <S.ImageWrapper>
+                  <S.ProductImage
+                    key={el._id}
+                    src={`https://storage.googleapis.com/${el.images?.[0]}`}
+                    onError={handleImageError}
+                  />
+                </S.ImageWrapper>
+                <S.Column>{el.name}</S.Column>
+                <S.ProductPrice>{putOnComma(el.price)}원</S.ProductPrice>
+                <S.Column>{el.remarks}</S.Column>
+              </S.Row>
+            ))
+          )}
         </S.CartBody>
       </S.CartWrapper>
     </S.Wrapper>
--- src/components/units/auth/cart/Cart.styles.ts
+++ src/components/units/auth/cart/Cart.styles.ts
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { GiShoppingCart } from 'react-icons/gi';
 
 export const Wrapper = styled.div`
   width: 100vw;
@@ -45,4 +46,36 @@ export const ProductPrice = styled.div`
 export const ProductImage = styled.img`
   width: 180px;
   height: 180px;
+`;
+
+export const EmptyCartWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 2rem;
+`;
+
+export const EmptyCartIcon = styled(GiShoppingCart)`
+  color: #000000d9;
+  font-size: 120px;
+  margin-bottom: 1rem;
+`;
+
+export const EmptyTitle = styled.h2`
+  color: #a0a0a0;
+  font-size: 32px;
+  letter-spacing: 0.5rem;
+`;
+
+export const DivideLine = styled.hr`
+  width: 50%;
+  margin-bottom: 1rem;
+`;
+
+export const EmptyCartSpan = styled.span`
+  color: #000000d9;
+  font-size: 22px;
+  text-align: center;
+  margin-bottom: 2rem;
 `;
