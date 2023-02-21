0a75b9c721bf5374eb6ba6db089fb81e0e9102ee
leedawnn <2g_hee@naver.com>
2023-02-22 02:05:17 +0900

    feat: _document.tsx 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,6 @@ import 'nprogress/nprogress.css';
 import Lottie from 'react-lottie';
 import animationData from '../public/animal-paw-loading.json';
 import styled from '@emotion/styled';
-import Head from 'next/head';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [loading, setLoading] = useState<boolean>(false);
@@ -47,17 +46,6 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     <>
-      <Head>
-        <title>오직 반려동물만을 위한 댕근마켓</title>
-        <meta name='description' content='중고 거래부터 정보 공유까지 반려인들과 함께해요.' />
-        <meta property='og:url' content='https://danggn.shop/'></meta>
-        <meta property='og:title' content='오직 반려동물만을 위한 댕근마켓'></meta>
-        <meta property='og:description' content='중고 거래부터 정보 공유까지 반려인들과 함께해요.'></meta>
-        <meta property='og:site_name' content='댕근마켓'></meta>
-        <meta property='og:image' content='/public/metaImage.png'></meta>
-        <meta property='og:type' content='article'></meta>
-        <meta property='og:locale' content='ko_KR'></meta>
-      </Head>
       {loading ? (
         <LoadingWrapper>
           <Lottie options={defaultOptions} width={300} height={300} />
