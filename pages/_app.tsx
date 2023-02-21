8ffab9a44b7e41943a63675c0cb4f345a42cdb66
leedawnn <2g_hee@naver.com>
2023-02-21 23:33:08 +0900

    conf: 메타 태그 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,11 +7,11 @@ import ApolloSetting from '../src/commons/settings/apolloSetting';
 import { RecoilRoot } from 'recoil';
 import { useEffect, useState } from 'react';
 import Router from 'next/router';
-import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
 import Lottie from 'react-lottie';
 import animationData from '../public/animal-paw-loading.json';
 import styled from '@emotion/styled';
+import Head from 'next/head';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [loading, setLoading] = useState<boolean>(false);
@@ -47,6 +47,17 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     <>
+      <Head>
+        <title>오직 반려동물만을 위한 댕근마켓</title>
+        <meta name='description' content='중고 거래부터 정보 공유까지 반려인들과 함께해요.' />
+        <meta property='og:url' content='https://danggn.shop/'></meta>
+        <meta property='og:title' content='오직 반려동물만을 위한 댕근마켓'></meta>
+        <meta property='og:description' content='중고 거래부터 정보 공유까지 반려인들과 함께해요.'></meta>
+        <meta property='og:site_name' content='댕근마켓'></meta>
+        <meta property='og:image' content='/public/metaImage.png'></meta>
+        <meta property='og:type' content='article'></meta>
+        <meta property='og:locale' content='ko_KR'></meta>
+      </Head>
       {loading ? (
         <LoadingWrapper>
           <Lottie options={defaultOptions} width={300} height={300} />
