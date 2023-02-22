1542e9d9d20744e07183aa370a2166731cdd07dc
leedawnn <2g_hee@naver.com>
2023-02-22 15:04:15 +0900

    refactor: _document.tsx 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,22 +4,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang='ko'>
-        <Head>
-          <title>오직 반려동물만을 위한, 댕근마켓</title>
-          <meta charSet='utf-8' />
-          <meta name='description' content='중고 거래부터 정보 공유까지 반려인들과 함께해요.' />
-          <meta property='og:url' content='https://danggn.shop/'></meta>
-          <meta property='og:title' content='오직 반려동물만을 위한, 댕근마켓'></meta>
-          <meta property='og:description' content='중고 거래부터 정보 공유까지 반려인들과 함께해요.'></meta>
-          <meta property='og:site_name' content='댕근마켓'></meta>
-          <meta property='og:image' content='/public/metaImage.png'></meta>
-          <meta property='og:type' content='article'></meta>
-          <meta property='og:locale' content='ko_KR'></meta>
-          <link
-            href='https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_three@1.1/SangSangShinb7.woff'
-            rel='stylesheet'
-          />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
